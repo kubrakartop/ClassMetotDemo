71b73fbe4b1c882733fc3a28f94c10a760be4f4a
PC <PC@RMOS>
2021-12-04 16:49:12 +0300

    Add project files.

---
+++ ClassMetotDemo/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Customer
+    {
+        public int ID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerLastname { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email {get; set; }
+
+        internal void Add(Customer customer1)
+        {
+            NotImplementedException notImplementedException = new NotImplementedException();
+     
+        }
+        
+        internal void Delete(Customer customer2)
+        {
+            NotImplementedException notImplementedException = new NotImplementedException();
+
+        }
+        internal void ListCustomer(Customer customer3)
+        {
+            NotImplementedException notImplementedException = new NotImplementedException();
+
+        }
+        
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using ClassMetotDemo;
+
+static void Add(Customer customer)
+{
+    Console.WriteLine(customer.ID + " " + customer.CustomerName + " " + customer.CustomerLastname + " " + customer.PhoneNumber + "" + customer.Email + " " + "Customer Information Added");
+}
+
+static void Delete(Customer customer)
+{
+    Console.WriteLine(customer.ID + " " + customer.CustomerName + " " + customer.CustomerLastname + " " + customer.PhoneNumber + "" + customer.Email + " " + "Customer Information Deleted");
+}
+
+static void ListCostumer(Customer customer)
+{
+    Console.WriteLine(customer.ID + " " + customer.CustomerName + " " + customer.CustomerLastname + " " + customer.PhoneNumber + "" + customer.Email + "" + "Customer Information");
+
+}
+Customer customer1=new Customer();
+customer1.ID = 1;
+customer1.CustomerName = "Sophia";
+customer1.CustomerLastname = "Brown";
+customer1.PhoneNumber = +1402221112;
+customer1.Email = "sophiab@gmail.com";
+
+Customer customer2 = new Customer();
+customer2.ID = 2;
+customer2.CustomerName = "Laya";
+customer2.CustomerLastname = "Hayes";
+customer2.PhoneNumber = +140281113;
+customer2.Email = "hayesl@gmail.com";
+
+Customer customer3 = new Customer();
+customer3.ID = 3;
+customer3.CustomerName = "David";
+customer3.CustomerLastname = "Stone ";
+customer3.PhoneNumber = +140268116;
+customer3.Email = "stone@gmail.com";
+
+Customer customerManeger=new Customer();
+customerManeger.Add(customer1);
+customerManeger.Delete(customer2);
+customerManeger.ListCustomer(customer3);
+
+Customer customerManager1 = new Customer();
+Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+foreach (var customer in customers)
+{
+    Console.WriteLine(customer.ID + " " + customer.CustomerName + " " + customer.CustomerLastname + " " + customer.PhoneNumber + " "+ customer.Email );
+
+}
